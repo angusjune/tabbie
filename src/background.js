905e60ed61cff1f28ca50fe61307c198ce1f67de
angusjune <angus.is.online@gmail.com>
2021-05-18 16:37:21 +0800

    Modernize service worker for MV3

--- src/background.js
+++ src/background.js
@@ -1,64 +1,34 @@
 'use strict';
 
-let darkMode         = 'auto';
-let itemLimit        = 10;
-let showSearch       = false;
-let showLastModified = true;
-let useTabs          = false;
+const setClosedSessions = () => {
+  chrome.storage.sync.get({ itemLimit: 10 }, props => {
+    const itemLimit = props.itemLimit;
 
-chrome.storage.sync.get({
-  darkMode: 'auto',
-  itemLimit: 10,
-  showSearch: false,
-  showLastModified: true,
-  useTabs: false
-}, result => {
-  darkMode  = result.darkMode;
-  itemLimit = result.itemLimit;
-
-  setClosedSessions(itemLimit);
-});
-
-const setClosedSessions = (itemLimit = 10) => {
-  chrome.sessions.getRecentlyClosed({maxResults: itemLimit}, sessions => {
-    const limitedSessions = sessions.splice(0, itemLimit);
-    chrome.storage.local.set({ closedSessions: limitedSessions });
+    chrome.sessions.getRecentlyClosed({maxResults: itemLimit}, sessions => {
+      const limitedSessions = sessions.splice(0, itemLimit);
+      chrome.storage.local.set({ closedSessions: limitedSessions });
+    });
   });
 };
 
+chrome.runtime.onStartup.addListener(() => {
+  setClosedSessions();
+});
+
 chrome.storage.onChanged.addListener((changes, namespace) => {
   if (changes.itemLimit) {
-    itemLimit = changes.itemLimit.newValue;
-    setClosedSessions(itemLimit);
+    setClosedSessions();
   }
 });
 
 chrome.tabs.onCreated.addListener(tab => {
-  setClosedSessions(itemLimit);
+  setClosedSessions();
 });
 
 chrome.tabs.onUpdated.addListener(tab => {
-  setClosedSessions(itemLimit);
+  setClosedSessions();
 });
 
 chrome.tabs.onRemoved.addListener(tab => {
-  setClosedSessions(itemLimit);
-});
-
-chrome.runtime.onMessage.addListener(message => {
-  if (message.isBrowserDark || darkMode === 'dark') {
-    chrome.action.setIcon({ 
-      path: {
-        16: `icons/action-icon-light-16.png`,
-        24: `icons/action-icon-light-24.png`,
-        32: `icons/action-icon-light-32.png`,
-      }
-    });
-  } else {
-    chrome.action.setIcon({ path: {
-      16: `icons/action-icon-16.png`,
-      24: `icons/action-icon-24.png`,
-      32: `icons/action-icon-32.png`,
-    }});
-  }
+  setClosedSessions();
 });
